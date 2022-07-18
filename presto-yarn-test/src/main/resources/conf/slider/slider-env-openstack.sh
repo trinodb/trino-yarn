23b8d1aa28cc14abea3b637283c4c854a2ed5960
duhanmin <hanmin.du@longbridge.sg>
2022-07-18 10:31:19 +0800

    add

--- presto-yarn-test/src/main/resources/conf/slider/slider-env-openstack.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export JAVA_HOME=/usr/lib/jvm/java
-export HADOOP_CONF_DIR=/etc/hadoop/conf
-
---
+++ src/main/java/com/trino/on/yarn/constant/Constants.java
@@ -0,0 +1,28 @@
+package com.trino.on.yarn.constant;
+
+/**
+ * Constants
+ */
+public class Constants {
+
+    public static final String SHELL_ARGS_PATH = "shellArgs";
+
+    public static final String JAVA_OPTS_PATH = "javaOpts";
+
+    public static final String JAR_FILE_LINKEDNAME = "jar";
+
+    public static final String APP_MASTER_JAR_PATH = "AppMaster.jar";
+
+    public static final String JAR_FILE_PATH = "JAR_FILE_PATH";
+
+    public static final String LOG_4_J_PATH = "log4j.properties";
+
+    public static final String DATAX = "datax";
+
+    public static final String DATAX_HOME = "/" + DATAX + "/" + DATAX +"/";
+
+    public static final String DATAX_JOB = "datax.job";
+
+    public static final String DATAX_SCRIPT_PYTHON = "#!/bin/bash\n/usr/bin/python %s/bin/datax.py --jvm=\"-Xms%dm -Xmx%dm\" %s";
+
+}
