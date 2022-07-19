bccc130b0524547e37f6c996e0c7c0c6bb9fe940
duhanmin <hanmin.du@longbridge.sg>
2022-07-19 15:04:14 +0800

    add Constants

--- src/main/java/com/trino/on/yarn/constant/Constants.java
+++ src/main/java/com/trino/on/yarn/constant/Constants.java
@@ -30,11 +30,121 @@ public class Constants {
 
     public static final String LOG_4_J_PATH = "log4j.properties";
 
-    public static final String DATAX = "datax";
+    public static final String TRINO_LOG = "log.properties";
 
-    public static final String DATAX_HOME = "/" + DATAX + "/" + DATAX + "/";
+    public static final String TRINO_LOG_CONTENT = "io.trino=INFO";
 
-    public static final String DATAX_JOB = "datax.job";
+    public static final String TRINO_JVM = "jvm.config";
+
+    public static final String TRINO_JVM_CONTENT = "-verbose:class\n" +
+            "-server\n" +
+            "-Xmx{}\n" +
+            "-XX:+UseG1GC\n" +
+            "-XX:G1HeapRegionSize=32M\n" +
+            "-XX:+ExplicitGCInvokesConcurrent\n" +
+            "-XX:+HeapDumpOnOutOfMemoryError\n" +
+            "-XX:OnOutOfMemoryError=kill -9 %p\n" +
+            "-XX:ReservedCodeCacheSize=150M\n" +
+            "-Dlog4j2.formatMsgNoLookups=true\n" +
+            "-Djava.library.path=/usr/lib/hadoop/lib/native/:/usr/lib/hadoop-lzo/lib/native/:/usr/lib/\n" +
+            "-Djdk.attach.allowAttachSelf=true\n" +
+            "--add-exports=java.base/jdk.internal.util=ALL-UNNAMED \n" +
+            "--add-opens=java.base/jdk.internal.util=ALL-UNNAMED\n" +
+            "--add-exports=java.base/jdk.internal.loader=ALL-UNNAMED\n" +
+            "--add-opens=java.base/jdk.internal.loader=ALL-UNNAMED\n" +
+            "--add-exports=java.base/jdk.internal.module=ALL-UNNAMED\n" +
+            "--add-opens=java.base/jdk.internal.module=ALL-UNNAMED\n" +
+            "--add-exports=java.base/java.lang.module=ALL-UNNAMED\n" +
+            "--add-opens=java.base/java.lang.module=ALL-UNNAMED\n" +
+            "--add-exports=java.base/jdk.internal.util.jar=ALL-UNNAMED\n" +
+            "--add-opens=java.base/jdk.internal.util.jar=ALL-UNNAMED\n" +
+            "--add-exports=java.base/jdk.internal.reflect=ALL-UNNAMED\n" +
+            "--add-opens=java.base/jdk.internal.reflect=ALL-UNNAMED\n" +
+            "--add-exports=java.base/jdk.internal.ref=ALL-UNNAMED\n" +
+            "--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED\n" +
+            "-Djava.rmi.server.hostname=localhost";
+
+    public static final String TRINO_ENV = "trino-env.sh";
+
+    /**
+     * JAVA11_HOME=$(ls -d /usr/lib/jvm/java-11-amazon-corretto.*)
+     * export JAVA_HOME=$JAVA11_HOME
+     * export PATH=$JAVA_HOME/bin:$PATH
+     * export LC_ALL=zh_CN.UTF-8
+     * export EXTRA_ARGS="--server http://ip-*-*-*-*.vpc.internal:8889"
+     */
+    public static final String TRINO_ENV_CONTENT = "export JAVA_HOME={}\n" +
+            "export PATH=$JAVA_HOME/bin:$PATH\n" +
+            "export LC_ALL=zh_CN.UTF-8\n" +
+            "export EXTRA_ARGS=\"--server http://{}:{}\"";
+
+    public static final String TRINO_NODE = "node.properties";
+
+    /**
+     * node.environment=production
+     * node.id=i-dsadsadasda
+     * node.data-dir=/var/lib/trino/data
+     * catalog.config-dir=/etc/trino/conf/catalog
+     * plugin.dir=/usr/lib/trino/plugin
+     */
+    public static final String TRINO_NODE_CONTENT = "node.environment=production\n" +
+            "node.id={}\n" +
+            "node.data-dir={}/data\n" +
+            "catalog.config-dir={}/conf/catalog\n" +
+            "plugin.dir={}";
+
+
+    public static final String TRINO_CONFIG = "config.properties";
+
+    /**
+     * coordinator=true
+     * node-scheduler.include-coordinator=true
+     * discovery.uri=http://ip-*-*-*-*.vpc.internal:8889
+     * http-server.threads.max=500
+     * discovery-server.enabled=true
+     * sink.max-buffer-size=1GB
+     * query.max-memory=22938MB \\单个查询在集群的最大内存
+     * query.max-memory-per-node=26401163969B \\单个查询在单个节点上的最大用户内存
+     * query.max-total-memory-per-node=31681396763B \\单个查询在单个节点上的最大用户和系统内存
+     * query.max-history=40
+     * query.min-expire-age=30m
+     * query.client.timeout=30m
+     * query.stage-count-warning-threshold=100
+     * query.max-stage-count=150
+     * http-server.http.port=8889
+     * http-server.log.path=/var/log/trino/http-request.log
+     * http-server.log.max-size=67108864B
+     * http-server.log.max-history=5
+     * log.max-size=268435456B
+     * log.max-history=5
+     * jmx.rmiregistry.port = 9080
+     * jmx.rmiserver.port = 9081
+     * scheduler.http-client.max-requests-queued-per-destination=5120
+     * scheduler.http-client.max-connections-per-server=5120
+     * exchange.http-client.request-timeout=50s
+     * exchange.http-client.max-requests-queued-per-destination=5120
+     * exchange.http-client.max-connections-per-server=5120
+     */
+    public static final String TRINO_CONFIG_CONTENT = "coordinator=true\n" +
+            "node-scheduler.include-coordinator=true\n" +
+            "discovery.uri={}:{}\n" +
+            "http-server.threads.max=500\n" +
+            "discovery-server.enabled=true\n" +
+            "sink.max-buffer-size=1GB\n" +
+            "query.max-memory={}\n" +
+            "query.max-memory-per-node={}\n" +
+            "query.max-total-memory-per-node=31681396763B\n" +
+            "query.max-history=40\n" +
+            "query.min-expire-age=30m\n" +
+            "query.client.timeout=30m\n" +
+            "query.stage-count-warning-threshold=100\n" +
+            "query.max-stage-count=150\n" +
+            "http-server.http.port={}\n" +
+            "http-server.log.path={}/http-request.log\n" +
+            "http-server.log.max-size=67108864B\n" +
+            "http-server.log.max-history=5\n" +
+            "log.max-size=268435456B\n" +
+            "log.max-history=5";
 
     public static final String DATAX_SCRIPT_PYTHON = "#!/bin/bash\n/usr/bin/python %s/bin/datax.py --jvm=\"-Xms%dm -Xmx%dm\" %s";
 
--- src/main/java/com/trino/on/yarn/executor/TrinoJdbc.java
+++ src/main/java/com/trino/on/yarn/executor/TrinoJdbc.java
@@ -1,23 +1,32 @@
 package com.trino.on.yarn.executor;
 
+import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.util.StrUtil;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Properties;
 
 public class TrinoJdbc {
 
     public static final String JDBC_TRINO = "jdbc:trino://{}:{}/hive";
 
     public static void run(String ip, Integer port, String email, String sql) throws ClassNotFoundException, SQLException {
-        Class.forName("io.trino.jdbc.TrinoDriver");
-        Properties properties = new Properties();
-        properties.setProperty("user", email);
-        Connection connection = DriverManager.getConnection(StrUtil.format(JDBC_TRINO, ip, port), properties);
-        Statement statement = connection.createStatement();
-        statement.execute(sql);
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            Class.forName("io.trino.jdbc.TrinoDriver");
+            if (StrUtil.isEmpty(email)) {
+                email = "root";
+            }
+            connection = DriverManager.getConnection(StrUtil.format(JDBC_TRINO, ip, port), email, null);
+            statement = connection.createStatement();
+            statement.execute(sql);
+        } finally {
+            IoUtil.close(statement);
+            IoUtil.close(connection);
+        }
+
     }
 }
