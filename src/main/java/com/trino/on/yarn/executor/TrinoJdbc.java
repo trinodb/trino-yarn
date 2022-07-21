a5341d700c192e1d7c8aebc2c793b1dc8b44151a
duhanmin <hanmin.du@longbridge.sg>
2022-07-21 15:53:15 +0800

    execute sql

--- src/main/java/com/trino/on/yarn/executor/TrinoJdbc.java
+++ src/main/java/com/trino/on/yarn/executor/TrinoJdbc.java
@@ -2,6 +2,8 @@ package com.trino.on.yarn.executor;
 
 import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.util.StrUtil;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,7 +11,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class TrinoJdbc {
-
+    protected static final Log LOG = LogFactory.getLog(TrinoJdbc.class);
     public static final String JDBC_TRINO = "jdbc:trino://{}:{}/hive";
 
     public static void run(String ip, Integer port, String email, String sql) throws ClassNotFoundException, SQLException {
@@ -22,7 +24,10 @@ public class TrinoJdbc {
             }
             connection = DriverManager.getConnection(StrUtil.format(JDBC_TRINO, ip, port), email, null);
             statement = connection.createStatement();
-            statement.execute(sql);
+            for (String tmp : StrUtil.split(sql, ";")) {
+                statement.execute(tmp);
+                LOG.warn("execute sql:" + tmp);
+            }
         } finally {
             IoUtil.close(statement);
             IoUtil.close(connection);
