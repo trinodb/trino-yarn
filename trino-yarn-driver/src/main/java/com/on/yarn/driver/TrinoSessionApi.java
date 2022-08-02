aec8d671139e793f3b8b3fd72540bdb98fbbfadc
duhanmin <hanmin.du@longbridge.sg>
2022-08-02 18:19:14 +0800

    测试用例

--- trino-yarn-driver/src/main/java/com/on/yarn/driver/TrinoSessionApi.java
+++ trino-yarn-driver/src/main/java/com/on/yarn/driver/TrinoSessionApi.java
@@ -1,14 +1,19 @@
 package com.on.yarn.driver;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.yarn.api.records.ApplicationId;
 import org.apache.hadoop.yarn.api.records.ApplicationReport;
+import org.apache.hadoop.yarn.util.Records;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TrinoSessionApi {
@@ -21,9 +26,20 @@ public class TrinoSessionApi {
             System.exit(1);
         }
         String url = "http://" + address + "/ws/v1/cluster/apps?state=RUNNING";
-        String json = HttpUtil.get(url);
-        JSONArray apps = JSONUtil.parseObj(json).getJSONObject("apps").getJSONArray("app");
-        List<ApplicationReport> applicationReports = JSONUtil.toList(apps, ApplicationReport.class);
+        String data = HttpUtil.get(url);
+        JSONArray apps = JSONUtil.parseObj(data).getJSONObject("apps").getJSONArray("app");
+        List<ApplicationReport> applicationReports = new ArrayList<>();
+        for (int i = 0; i < apps.size(); i++) {
+            JSONObject json = apps.getJSONObject(i);
+            ApplicationReport report = Records.newRecord(ApplicationReport.class);
+            report.setApplicationId(ApplicationId.fromString(json.getStr("id")));
+            report.setApplicationType(json.getStr("applicationType"));
+            if (json.containsKey("applicationTags")) {
+                List<String> applicationType = json.getJSONArray("applicationTags").toList(String.class);
+                report.setApplicationTags(CollUtil.newHashSet(applicationType));
+            }
+            applicationReports.add(report);
+        }
         return TrinoSessionBase.getMeta(conf, applicationReports);
     }
 }
